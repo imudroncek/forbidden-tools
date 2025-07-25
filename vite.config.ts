640aaff1fe853aff2e52227adef9dcdeaea211e1
xmudronc <44iwan44@gmail.com>
2025-07-25 12:22:25 +0200

    Switching template.

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,22 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+import manifest from './manifest.json';
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  build: {
-    sourcemap: true,
-    assetsDir: "code",
-    target: ["esnext"],
-    cssMinify: true,
-    lib: false
-  },
   plugins: [
+    react(),
     VitePWA({
-      strategies: "injectManifest",
-      injectManifest: {
-        swSrc: 'public/sw.js',
-        swDest: 'dist/sw.js',
-        globDirectory: 'dist',
-        globPatterns: [
-          '**/*.{html,js,css,json,png}',
-        ],
-      },
-      injectRegister: false,
-      manifest: false,
-      devOptions: {
-        enabled: true
-      }
-    })
-  ]
-})
+      manifest,
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      // switch to "true" to enable sw on development
+      devOptions: { enabled: false },
+      registerType: 'autoUpdate',
+      workbox: { globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'] },
+    }),
+  ],
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+});
