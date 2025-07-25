ebcfd3732a75015d709027514e25a457ae2e7ae4
xmudronc <44iwan44@gmail.com>
2025-07-25 12:49:34 +0200

    Switching template again.

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,36 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
 
-import manifest from './manifest.json';
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest,
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      // switch to "true" to enable sw on development
-      devOptions: { enabled: false },
-      registerType: 'autoUpdate',
-      workbox: { globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'] },
-    }),
-  ],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-});
+  plugins: [preact(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'forbidden-tools',
+      short_name: 'forbidden-tools',
+      description: 'GM tools for Forbidden Lands TRPG',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
